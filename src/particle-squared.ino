2645169766b272487b0feba9718964b1a841b68c
Jared Wolff <jaredwolff@me.com>
2019-04-27 21:57:18 -0400

    Cleaning up main. Concatinating results. As the device scales, so can the JSON output.

--- src/particle-squared.ino
+++ src/particle-squared.ino
@@ -32,7 +32,7 @@ static uint32_t m_reading_period = MEASUREMENT_DELAY_MS;
 
 // Set up timer
 Timer timer(m_reading_period, timer_handler);
-Timer hpmatimer(HPMA_TIMEOUT_MS, hpma_timeout_handler, true);
+Timer hpma_timer(HPMA_TIMEOUT_MS, hpma_timeout_handler, true);
 
 // Watchdog
 ApplicationWatchdog wd(WATCHDOG_TIMEOUT_MS, System.reset);
@@ -49,12 +49,12 @@ static hpma115_data_t hpma115_data;
 static bool data_check = false;
 
 // Data state ready
-static bool hpma115_data_ready = false;
-static bool si7021_data_ready = false;
-static bool ccs811_data_ready = false;
+static bool m_data_ready = false;
 
 // State of baseline
 static uint32_t m_day_counter = 0;
+
+static String m_out;
 
 // Definition of timer handler
 void timer_handler() {
@@ -68,6 +68,9 @@ void hpma_timeout_handler() {
     Particle.publish("err", "hpma timeout" , PRIVATE, NO_ACK);
     hpma115.disable();
   }
+
+  m_out = String( m_out + "}" );
+  m_data_ready = true;
 }
 
 // ccs811_pin_interrupt() forwards pin interrupt on to the specific handler
@@ -86,7 +89,7 @@ void hpma_evt_handler(hpma115_data_t *p_data) {
   // Disable HPMA
   #ifdef HAS_HPMA
   hpma115.disable();
-  hpmatimer.stop();
+  hpma_timer.stop();
   #endif
 
   // Copy the data.
@@ -94,7 +97,11 @@ void hpma_evt_handler(hpma115_data_t *p_data) {
 
   // Serial.printf("pm25 %dμg/m3 pm10 %dμg/m3\n", hpma115_data.pm25, hpma115_data.pm10);
 
-  hpma115_data_ready = true;
+  // Concat the data into the json blob
+  m_out = String( m_out + String::format(",\"pm25\":%d}", hpma115_data.pm25) );
+
+  // Set the data ready!
+  m_data_ready = true;
 
   Serial.println("hpma rdy");
 }
@@ -172,6 +179,8 @@ void setup() {
     System.reset();
   }
 
+
+  #ifdef HAS_HPMA
   // Setup HPMA115
   hpma115_init_t hpma115_init;
   hpma115_init.callback = hpma_evt_handler;
@@ -184,6 +193,7 @@ void setup() {
     Serial.flush();
     System.reset();
   }
+  #endif
 
   // Start the timer
   timer.start();
@@ -196,6 +206,8 @@ void setup() {
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
+
+  uint32_t err_code;
 
   // Connect if not connected..
   #if PLATFORM_ID == PLATFORM_XENON
@@ -210,16 +222,14 @@ void loop() {
   #endif
 
   // If all the data is ready, send it as one data blob
-  if (ccs811_data_ready && si7021_data_ready && hpma115_data_ready ) {
+  if ( m_data_ready ) {
     Serial.println("data send");
 
-    String out = String::format("{\"temperature\":%.2f,\"humidity\":%.2f,\"pm25\":%d,\"pm10\":%d,\"tvoc\":%d,\"c02\":%d}",si7021_data.temperature,si7021_data.humidity,hpma115_data.pm25,hpma115_data.pm10,ccs811_data.tvoc,ccs811_data.c02);
-    Particle.publish("blob", out , PRIVATE, WITH_ACK);
+    // Publish data
+    Particle.publish("blob", m_out , PRIVATE, WITH_ACK);
 
     // Reset to false
-    ccs811_data_ready = false;
-    si7021_data_ready = false;
-    hpma115_data_ready = false;
+    m_data_ready = false;
   }
 
   // If we're greater than or equal to the measurement delay
@@ -229,19 +239,23 @@ void loop() {
     // Set state variable to false
     data_check = false;
 
+    // Set start of string.
+    m_out = String("{");
+
     // Disable HPMA
     #ifdef HAS_HPMA
     hpma115.disable();
     #endif
 
     // Read temp and humiity
-    uint32_t err_code = si7021.read(&si7021_data);
+    err_code = si7021.read(&si7021_data);
 
     if( err_code == SI7021_SUCCESS ) {
       // Set env data in the CCS811
           ccs811.set_env(si7021_data.temperature,si7021_data.humidity);
 
-      si7021_data_ready = true;
+      // Concatinate temp and humidity data
+      m_out = String( m_out + String::format("\"temperature\":%.2f,\"humidity\":%.2f",si7021_data.temperature, si7021_data.humidity) );
       Serial.println("temp rdy");
     } else {
       Particle.publish("err", "temp" , PRIVATE, NO_ACK);
@@ -252,8 +266,9 @@ void loop() {
     err_code = ccs811.read(&ccs811_data);
 
     if ( err_code == CCS811_SUCCESS ) {
-      ccs811_data_ready = true;
 
+      // Concatinate ccs811 tvoc
+      m_out = String( m_out + String::format(",\"tvoc\":%d", ccs811_data.tvoc) );
       Serial.println("tvoc rdy");
     } else {
       Particle.publish("err", "tvoc" , PRIVATE, NO_ACK);
@@ -266,9 +281,10 @@ void loop() {
     // (extends the life of the device)
     #ifdef HAS_HPMA
     hpma115.enable();
-    hpmatimer.start();
+    hpma_timer.start();
     #else
-    hpma115_data_ready = true;
+    m_out = String( m_out + "}");
+    m_data_ready = true;
     #endif
 
   }
