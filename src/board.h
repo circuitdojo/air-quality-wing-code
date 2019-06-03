833de98f104037bf92611f6326c65030dcd919ae
Jared Wolff <jaredwolff@me.com>
2019-06-03 08:50:48 -0400

    Rev3 has the reset pin relocated. Updatin board file with defaults.

--- src/board.h
+++ src/board.h
@@ -1,6 +1,16 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+// Hardware version types
+#define HW_VER1 1
+#define HW_VER2 2
+#define HW_VER3 3
+
+// Default version to 2
+#ifndef HW_VERSION
+#define HW_VERSION HW_VER2
+#endif
+
 #define HAS_HPMA
 // #define HAS_SGP30
 // #define HAS_BME680
@@ -11,7 +21,15 @@
 
 #define CCS811_WAKE_PIN D6
 #define CCS811_INT_PIN  D8
+
+// Reset is moved to D4 for Rev3
+#if HW_VERSION == HW_VER2 || HW_VERSION == HW_VER1
 #define CCS811_RST_PIN  D7
+#elif HW_VERSION == HW_VER3
+#define CCS811_RST_PIN  D4
+#else
+#error HW_VERSION must be set!
+#endif
 
 #define HPMA1150_EN_PIN D5
 
